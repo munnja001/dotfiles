831165c22cd5edfc6392aa0d9155f2de29e0acdb
Justin Munn <munnja001@gmail.com>
2012-04-29 18:50:54 -0500

    updated dotfiles to use zsh and the andre's version of organizing files

---
+++ system/aliases.zsh
@@ -0,0 +1,27 @@
+# ls aliases
+#alias ls="ls -F --color"
+#alias l="ls -lh"
+#alias ll="ls -lAh"
+#alias la="ls -A"
+
+# easy navigating
+alias ..="cd .."
+alias ...="cd ../.."
+
+# history alias
+alias hall='history 1 -1'
+alias h='history -30 -1'
+alias hg='history 1 -1 | grep '
+
+# process list helpers
+alias pg='ps -A | grep '
+alias psa='ps -A'
+
+# open current window in path finder
+alias o='open -a "Path Finder.app" .'
+
+# atea (https://github.com/pkamenarsky/atea)
+alias t='vim /Users/Andre/Dropbox/Random/tasks.txt'
+
+# copies public ssh key to clipboard
+alias pubkey="more ~/.ssh/id_dsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
